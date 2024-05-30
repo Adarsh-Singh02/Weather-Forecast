5b52f98aeebd235e4863f2714e01dd8fc3be2936
Adarsh-Singh02 <adarsh021324@gmail.com>
2024-05-30 20:39:27 +0530

    added the file to weather forecast

---
+++ Weather Forecast.py
@@ -0,0 +1,20 @@
+# Import the necessary modules!
+import requests
+
+# Input the city name
+city = input('Enter City name: ')
+
+# Or you can also hard-code the value
+# city = 'Irkutsk'
+
+# Display the message
+print('Displaying Weather report for: ' + city)
+
+# Fetch the weather details
+url = 'https://wttr.in/{}'.format(city) # Use of format to pass city as a parameter here
+res = requests.get(url)                 # Make use of the requests module
+
+# Display the result
+# Resultant data is stored in res. 
+# Use of the text method to extract our desired weather details to display the result
+print(res.text)
